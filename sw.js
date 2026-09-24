2327460e7214503f4c68d627a607ceedf0b8bc83
husucabi69 <husucabi69@gmail.com>
2026-09-24 15:58:13 +0900

    v7.4: cache shoulder clinical module datasets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX='muscle-atlas-chatgpt-';
-const CACHE=CACHE_PREFIX+'v7.3-schema-v1';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/media-v1.json','./data/knowledge-core-v1.json','./data/regions-v1.json','./data/schema/msk-knowledge-schema-v1.json'];
+const CACHE=CACHE_PREFIX+'v7.4-shoulder-exam-us';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/media-v1.json','./data/knowledge-core-v1.json','./data/regions-v1.json','./data/schema/msk-knowledge-schema-v1.json','./data/examination-shoulder-v1.json','./data/ultrasound-shoulder-v1.json','./data/quiz-shoulder-v1.json'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
