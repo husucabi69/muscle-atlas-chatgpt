0efccf3ae5063c823e04ea47d4df797d825331ff
husucabi69 <husucabi69@gmail.com>
2026-09-24 14:48:30 +0900

    v7.1: activate fixed-media cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='muscle-atlas-chatgpt-';
-const CACHE=CACHE_PREFIX+'v7-quiz';
+const CACHE=CACHE_PREFIX+'v7.1-fixed-media';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/media-v1.json'];
 
 self.addEventListener('install',e=>{
