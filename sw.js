5f80f031bf1b539107349e690d58fcc8e20bfe37
husucabi69 <husucabi69@gmail.com>
2026-09-24 15:02:01 +0900

    v7.2: activate upper-limb adaptive quiz cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='muscle-atlas-chatgpt-';
-const CACHE=CACHE_PREFIX+'v7.1-fixed-media';
+const CACHE=CACHE_PREFIX+'v7.2-upperlimb-adaptive-quiz';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/media-v1.json'];
 
 self.addEventListener('install',e=>{
