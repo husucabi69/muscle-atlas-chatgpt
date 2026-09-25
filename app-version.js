91e6a1e9815f265bbe77be538477f23715863717
husucabi69 <husucabi69@gmail.com>
2026-09-25 20:41:23 +0900

    stage16: bump app release version

--- app-version.js
+++ app-version.js
@@ -1,8 +1,8 @@
 (function(scope){
   const release=Object.freeze({
-    buildVersion:'2026.09.25-stage15.1',
-    displayVersion:'v10.0 · Stage 15 Auto Update',
-    cacheKey:'20260925-stage15-1'
+    buildVersion:'2026.09.25-stage16.1',
+    displayVersion:'v10.1 · Stage 16 Hierarchical Navigation',
+    cacheKey:'20260925-stage16-1'
   });
   scope.LYS_APP_RELEASE=release;
 })(typeof self!=='undefined'?self:window);
