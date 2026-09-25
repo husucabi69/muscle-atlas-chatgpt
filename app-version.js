d837b36d0c585254fbbe0fc8a9a5d27aa3bd4721
husucabi69 <husucabi69@gmail.com>
2026-09-25 19:15:40 +0900

    stage15: add canonical app release version

---
+++ app-version.js
@@ -0,0 +1,8 @@
+(function(scope){
+  const release=Object.freeze({
+    buildVersion:'2026.09.25-stage15.1',
+    displayVersion:'v10.0 · Stage 15 Auto Update',
+    cacheKey:'20260925-stage15-1'
+  });
+  scope.LYS_APP_RELEASE=release;
+})(typeof self!=='undefined'?self:window);
