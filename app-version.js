af47166bb038cdfa44a0b2ea9b5d09fdda33af16
husucabi69 <husucabi69@gmail.com>
2026-09-25 20:48:00 +0900

    stage17: bump app release version

--- app-version.js
+++ app-version.js
@@ -1,8 +1,8 @@
 (function(scope){
   const release=Object.freeze({
-    buildVersion:'2026.09.25-stage16.1',
-    displayVersion:'v10.1 · Stage 16 Hierarchical Navigation',
-    cacheKey:'20260925-stage16-1'
+    buildVersion:'2026.09.25-stage17.1',
+    displayVersion:'v10.2 · Stage 17 Muscle Illustration Audit',
+    cacheKey:'20260925-stage17-1'
   });
   scope.LYS_APP_RELEASE=release;
 })(typeof self!=='undefined'?self:window);
