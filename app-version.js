3de053616e37d0cbea8042f468fa5bd3d2b0fe9d
husucabi69 <husucabi69@gmail.com>
2026-09-26 11:42:37 +0900

    stage17: bump anterior deep cervical anatomy release

--- app-version.js
+++ app-version.js
@@ -1,8 +1,8 @@
 (function(scope){
   const release=Object.freeze({
-    buildVersion:'2026.09.26-stage17.17',
-    displayVersion:'v11.8 · Stage 17 Chest & Respiratory Anatomy',
-    cacheKey:'20260926-stage17-17'
+    buildVersion:'2026.09.26-stage17.18',
+    displayVersion:'v11.9 · Stage 17 Anterior Deep Cervical Anatomy',
+    cacheKey:'20260926-stage17-18'
   });
   scope.LYS_APP_RELEASE=release;
 })(typeof self!=='undefined'?self:window);
